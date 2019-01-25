1e7ae92b386043cb2de6630da549c0c3e6f03673
Martin <mcbride.martin@gmail.com>
2019-01-25 21:08:34 +0000

    Setup V0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setup(name='generativepy',
-      version='0.2',
+      version='0.3',
       url='https://github.com/martinmcbride/generativepy',
       license='MIT',
       author='Martin McBride',
