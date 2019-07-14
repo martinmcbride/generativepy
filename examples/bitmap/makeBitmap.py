31699fceb2fa1329b509584327f98258ec8acc74
Martin <mcbride.martin@gmail.com>
2019-07-14 12:13:10 +0100

    Add bitmap scaling and orientation

--- examples/bitmap/makeBitmap.py
+++ examples/bitmap/makeBitmap.py
@@ -1,12 +1,12 @@
-from generativepy.bitmap import makeBitmapImage
+from generativepy.bitmap import makeBitmapImage, BM_ROWCOL
 
-
-def draw(img, pixelSize, channels):
+def draw(img, scaling):
     img[:,:] = 1
-    img[:50, :50] = (1, 0, 0)
-    img[50:100, :50] = (0, 1, 0)
-    img[100:150, :50] = (0, 0, 1)
+    start = scaling.u2p([0.5, 0.1])
+    end = scaling.u2p([2.75, 1.1])
+    img[start[0]:end[0], start[1]:end[1]] = (1, 0, 0)
     return img
 
 
-makeBitmapImage("/tmp/makeBitmapImage.png", draw, pixelSize=(300, 200))
+makeBitmapImage("/tmp/makeBitmapImage.png", draw, pixelSize=(300, 200), width=3, height=2)
+makeBitmapImage("/tmp/makeBitmapImage2.png", draw, pixelSize=(300, 200), width=3, height=2, orientation=BM_ROWCOL)
--- generativepy/bitmap.py
+++ generativepy/bitmap.py
@@ -6,12 +6,19 @@
 import numpy as np
 from generativepy import movie
 
+# Orienatation
+BM_ROWCOL = 0   #Left to right, top to bottom
+BM_IMAGE = 1    #Top to bottom, left to right
+
+
 class BitmapScaling():
 
-    def __init__(self, pixelSize, width=None, height=None, startX=0, startY=0):
+    def __init__(self, pixelSize, width=None, height=None, startX=0, startY=0, channels=3, orientation=BM_IMAGE):
         self.pixelSize = pixelSize
         self.startX = startX
         self.startY = startY
+        self.channels = channels
+        self.orientation = orientation
 
         self.width = width
         self.height = height
@@ -23,12 +30,21 @@ class BitmapScaling():
         elif not width:
             self.width = height * pixelSize[0] / pixelSize[1]
 
-    def p2u(self, pixel):
-        ux =
+    def p2u(self, pos):
+        x = self.startX + pos[0]*self.width/self.pixelSize[0]
+        y = self.startY + pos[1]*self.height/self.pixelSize[1]
+        return x, y
+
+    def u2p(self, pos):
+        x = (pos[0] - self.startX)*self.pixelSize[0]/self.width
+        y = (pos[1] - self.startY)*self.pixelSize[1]/self.height
+        if 0 <= x < self.pixelSize[0] and 0 <= y < self.pixelSize[1]:
+           return int(x), int(y)
+        return None
 
 
 def makeBitmapImage(outfile, draw, pixelSize, width=None, height=None,
-              startX=0, startY=0, channels=3):
+              startX=0, startY=0, channels=3, orientation=BM_IMAGE):
     '''
     Create a PNG file using numpy
     :param outfile: Name of output file
@@ -39,7 +55,16 @@ def makeBitmapImage(outfile, draw, pixelSize, width=None, height=None,
     '''
     shape = (pixelSize[0], pixelSize[1], channels)
     array = np.zeros(shape)
-    array = draw(array, pixelSize, channels)
+    if not height and not width:
+        width = pixelSize[0]
+        height = pixelSize[1]
+    elif not height:
+        height = width * pixelSize[1] / pixelSize[0]
+    elif not width:
+        width = height * pixelSize[0] / pixelSize[1]
+    scaling = BitmapScaling(pixelSize, width, height, startX, startY, channels, orientation)
+    array = draw(array, scaling)
     img = np.clip(array*256, 0, 255).astype(np.uint8)
-    img = img.transpose(1, 0, 2)
+    if orientation==BM_IMAGE:
+        img = img.transpose(1, 0, 2)
     movie.saveFrame(outfile, img)
