d22ea680ab80c843b69d5103fdfe85f6847347d8
Martin <mcbride.martin@gmail.com>
2020-07-26 15:23:41 +0100

    Add simple  example

---
+++ examples/start/simpleimage.py
@@ -0,0 +1,20 @@
+from generativepy.drawing import makeImage, setup, text
+from generativepy.color import Color
+
+
+def draw(ctx, pixel_width, pixel_height, frame_no, frame_count):
+
+    setup(ctx, pixel_width, pixel_height, width=5, background=Color(0.4))
+
+    ctx.set_source_rgba(*Color(0.5, 0, 0))
+    ctx.rectangle(0.5, 0.5, 2.5, 1.5)
+    ctx.fill()
+
+    ctx.set_source_rgba(*Color(0, 0.75, 0, 0.5))
+    ctx.rectangle(2, 0.25, 2.5, 1)
+    ctx.fill()
+
+    text(ctx, "Simple Image", 1, 3, size=0.5, color=Color('cadetblue'), font='Arial')
+
+
+makeImage("/tmp/simpleimage.png", draw, 500, 400)
