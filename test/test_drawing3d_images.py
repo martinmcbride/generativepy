c9f0e246a2fdff393da8430ff5bc5aa92f1d1cd2
Martin <mcbride.martin@gmail.com>
2021-01-24 11:17:54 +0000

    Basic unit test for drawing3d

--- test/test_drawing3d_images.py
+++ test/test_drawing3d_images.py
@@ -11,41 +11,45 @@ class TestDrawing3dImages(unittest.TestCase):
             prog = ctx.program(
                 vertex_shader='''
                     #version 330
-
                     in vec2 in_vert;
                     in vec3 in_color;
-
-                    out vec3 v_color;
-
+                    out vec3 v_color;    // Goes to the fragment shader
                     void main() {
-                        v_color = in_color;
                         gl_Position = vec4(in_vert, 0.0, 1.0);
+                        v_color = in_color;
                     }
                 ''',
                 fragment_shader='''
                     #version 330
-
                     in vec3 v_color;
-
-                    out vec3 f_color;
-
+                    out vec4 f_color;
                     void main() {
-                        f_color = v_color;
+                        // We're not interested in changing the alpha value
+                        f_color = vec4(v_color, 1.0);
                     }
                 ''',
             )
 
-            x = np.linspace(-1.0, 1.0, 50)
-            y = np.random.rand(50) - 0.5
-            r = np.ones(50)
-            g = np.zeros(50)
-            b = np.zeros(50)
+            # Point coordinates are put followed by the vec3 color values
+            vertices = np.array([
+                # x, y, red, green, blue
+                0.0, 0.8, 1.0, 0.0, 0.0,
+                -0.6, -0.8, 0.0, 1.0, 0.0,
+                0.6, -0.8, 0.0, 0.0, 1.0,
+            ], dtype='f4')
 
-            vertices = np.dstack([x, y, r, g, b])
+            vbo = ctx.buffer(vertices)
 
-            vbo = ctx.buffer(vertices.astype('f4').tobytes())
-            vao = ctx.simple_vertex_array(prog, vbo, 'in_vert', 'in_color')
+            # We control the 'in_vert' and `in_color' variables
+            vao = ctx.vertex_array(
+                prog,
+                [
+                    # Map in_vert to the first 2 floats
+                    # Map in_color to the next 3 floats
+                    (vbo, '2f 3f', 'in_vert', 'in_color')
+                ],
+            )
 
-            vao.render(moderngl.LINE_STRIP)
+            vao.render(moderngl.TRIANGLES)
 
         self.assertTrue(run_image3d_test('test_simple_drawing3d.png', draw, 700, 600, Color('grey')))
