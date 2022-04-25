3bb72eda8c05c0d9461a1b54cbb80a5209fda282
Martin <mcbride.martin@gmail.com>
2022-04-25 20:00:55 +0100

    Add MovieBuilder

---
+++ examples/movie/moviebuilder-no-audio.py
@@ -0,0 +1,50 @@
+from generativepy.geometry import Circle, Rectangle
+from generativepy.color import Color
+from generativepy.drawing import setup, make_image_frames
+from generativepy.movie import MovieBuilder
+from generativepy.tween import TweenVector
+from generativepy.utils import temp_file
+
+# Create a movie with two scenes and audio files
+WIDTH = 200         # Nominal width, used for all drawing
+HEIGHT = 300        # Nominal width, used for all drawing
+OUTPUTWIDTH = 200   # Final video width, could be made larger for 4k output
+OUTPUTHEIGHT = 300  # Final video height, could be made larger for 4k output
+
+FRATE = 10
+VIDEONAME = temp_file("movie-no-audio.mp4")
+
+def t2f(t):   # Convert a time in seconds to a number of frames
+    return int(t*FRATE)
+
+def scene1(duration):
+
+    position = TweenVector((0, 0)).to((200, 300), t2f(duration))
+
+    def draw(ctx, pixel_width, pixel_height, fn, frame_count):
+        setup(ctx, pixel_width, pixel_height, width=OUTPUTWIDTH, height=OUTPUTHEIGHT, background=Color(0.5))
+
+        Circle(ctx).of_center_radius(position[fn], 30).fill(Color("red"))
+
+    return make_image_frames(draw, WIDTH, HEIGHT, t2f(duration)), duration
+
+def scene2(duration):
+
+    position = TweenVector((0, 0)).to((200, 300), t2f(duration))
+
+    def draw(ctx, pixel_width, pixel_height, fn, frame_count):
+        setup(ctx, pixel_width, pixel_height, width=OUTPUTWIDTH, height=OUTPUTHEIGHT, background=Color(0.5))
+
+        Rectangle(ctx).of_corner_size(position[fn], 50, 50).fill(Color("green"))
+
+    return make_image_frames(draw, WIDTH, HEIGHT, t2f(duration)), duration
+
+
+
+builder = MovieBuilder(FRATE)
+builder.add_clip(scene1(5))
+builder.add_clip(scene2(5))
+
+# builder.make_movie(VIDEONAME, n) creates video of just scene n
+# builder.make_movie(VIDEONAME) creates full video
+builder.make_movie(VIDEONAME)
--- generativepy/movie.py
+++ generativepy/movie.py
@@ -3,9 +3,15 @@
 # Copyright (C) 2018, Martin McBride
 # License: MIT
 
-import cairo
 import numpy as np
 from PIL import Image
+from generativepy.utils import temp_file
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.editor import VideoClip
+from moviepy.video.compositing.concatenate import concatenate_videoclips
+import subprocess as sp
+import pathlib
+
 
 '''
 The movie functions operate pn lazy sequences of images. The images are stored as numpy arrays.
@@ -59,3 +65,92 @@ def save_frames(outfile, frames):
         image = Image.fromarray(normalise_array(frame))
         image.save(outfile + str(i).zfill(8) + '.png')
 
+
+def create_videoclip(frames, duration, frame_rate, audio_in=None):
+    """
+    Create a VideoClip object
+    :param frames: a iterator returning numpy frame objects
+    :param duration: Duration of clip in seconds
+    :param audio_in: file name of audio file, or None
+    :return:
+    """
+
+    def make_frame(t):
+        nonlocal current_frame
+        nonlocal current_frame_index
+        required_frame_index = int(t*frame_rate)
+        if required_frame_index > current_frame_index:
+            current_frame = next(frames)
+            current_frame_index += 1
+        rgb_frame = np.empty((current_frame.shape[0], current_frame.shape[1], 3), dtype=np.uint8)
+        rgb_frame[:, :] = current_frame[:, :, 0:3]
+        return rgb_frame
+
+    current_frame = next(frames)
+    current_frame_index = 0
+    video_clip = VideoClip(make_frame, duration=duration)
+    if audio_in:
+        print("Adding audio clip", audio_in)
+        audio_clip = AudioFileClip(audio_in).subclip(0, duration)
+        video_clip = video_clip.set_audio(audio_clip)
+    return video_clip
+
+
+class MovieBuilder():
+    """
+    Builds up a movie
+    """
+
+    def __init__(self, frame_rate):
+        self.frame_rate = frame_rate
+        self.frame_sources = []
+        self.audio_files = []
+        self.duration = []
+
+    def add_clip(self, frame_source_duration, audio_file=None):
+        """
+        Add a clip
+        :param frame_source_duration: Tuple of (frame_source, duration). frame_source is a iterator returning
+        numpy frame objects, duration is the clip duration in seconds
+        :param audio_file:
+        :return:
+        """
+        self.frame_sources.append(frame_source_duration[0])
+        self.duration.append(frame_source_duration[1])
+        self.audio_files.append(audio_file)
+
+    def make_movie(self, video_out, source=None):
+        """
+        Make a movie of either all the clips that have been added, or just a single clip if source is not None
+        :param video_out: Filename of output file.
+        :param source: set to index of a clip to use just that clip, or None to join all clips
+        :return:
+        """
+        if source is not None:
+            video = create_videoclip(self.frame_sources[source], self.duration[source], self.frame_rate, self.audio_files[source])
+        else:
+            clips = [create_videoclip(s, d, self.frame_rate, a) for s, d, a in zip(self.frame_sources, self.duration, self.audio_files)]
+            video = concatenate_videoclips(clips)
+
+        # Due to a bug in moviepy 1.0.1, when we write a video out in this mode the audio is not included.
+        # So we write the video and audio out to separate temporary files.
+        # We then use ffmpeg directly to combine the video and audio.
+        temp_video_filename = temp_file(pathlib.Path(video_out).stem + "TEMP.mp4")
+        temp_audio_filename = temp_file(pathlib.Path(video_out).stem + "TEMP.m4a")
+
+        if not self.audio_files[0]:
+            video.write_videofile(video_out, codec="libx264", fps=self.frame_rate)
+        else:
+            video.write_videofile(temp_video_filename, temp_audiofile=temp_audio_filename, codec="libx264",
+                                  remove_temp=False, audio_codec="aac", fps=self.frame_rate)
+
+            command = ["ffmpeg",
+                       "-y", #approve output file overwite
+                       "-i", temp_video_filename,
+                       "-i", temp_audio_filename,
+                       "-c:v", "copy",
+                       "-c:a", "copy",
+                       "-shortest",
+                       "-r", str(self.frame_rate),
+                       video_out ]
+            process = sp.Popen(command)
