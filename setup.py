81213dc6d6c454a13fdb38131374c50b88450c86
Martin <mcbride.martin@gmail.com>
2022-04-27 21:48:54 +0100

    Update for version 3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setup(name='generativepy',
-      version='3.0',
+      version='3.1',
       url='https://github.com/martinmcbride/generativepy',
       license='MIT',
       author='Martin McBride',
