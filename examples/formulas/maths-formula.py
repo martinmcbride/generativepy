d05648a4cab96ce6f4b6196cebbf26995314597f
Martin <mcbride.martin@gmail.com>
2022-08-30 11:58:59 +0100

    Update formulas example to to include latex packages

--- examples/formulas/maths-formula.py
+++ examples/formulas/maths-formula.py
@@ -7,14 +7,18 @@ from generativepy.formulas import rasterise_formula
 Create some formulae
 '''
 
-def draw(ctx, width, height, frame_no, frame_count):
-    setup(ctx, width, height, width=800, background=Color(0.8))
+def draw(ctx, pixel_width, pixel_height, frame_no, frame_count):
+    setup(ctx, pixel_width, pixel_height, background=Color(0.8))
 
     image1, size1 = rasterise_formula("cosh-formula", r"\cosh{x} = \frac{e^{x}+e^{-x}}{2}", Color("dodgerblue"))
     image2, size2 = rasterise_formula("quadratic", r"x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}", Color("crimson"), dpi=400)
+    image3, size3 = rasterise_formula("formula-test-temp", r"\frac{a\cancel{b}}{\cancel{b}}\si{kg.m/s^2}",
+                                       Color("black"), dpi=400, packages=["cancel", "siunitx"])
     Image(ctx).of_file_position(image1, (50, 50)).paint()
     Text(ctx).of("Size1 = " + str(size1), (50, 500)).size(20).fill(Color(0))
     Image(ctx).of_file_position(image2, (50, 300)).paint()
     Text(ctx).of("Size2 = " + str(size2), (250, 500)).size(20).fill(Color(0))
+    Image(ctx).of_file_position(image3, (650, 300)).paint()
+    Text(ctx).of("Size3 = " + str(size3), (450, 500)).size(20).fill(Color(0))
 
-make_image("/tmp/maths-formula.png", draw, 800, 600)
+make_image("/tmp/maths-formula.png", draw, 1000, 600)
