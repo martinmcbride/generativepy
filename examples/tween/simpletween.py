b94a304f4ca2dfa2eca31e4e2ba256ef6f49d4fb
Martin <mcbride.martin@gmail.com>
2023-01-23 18:30:31 +0000

    Update tween example

--- examples/tween/simpletween.py
+++ examples/tween/simpletween.py
@@ -1,15 +1,19 @@
-from generativepy.tween import Tween
+from generativepy.tween import Tween, set_frame_rate
 
-# Create a tween
-tween = Tween(3.0).wait(4).to(13.0, 5).wait(2).set(7.0, 4)
+# Set global frame rate to 4
+set_frame_rate(4)
+
+# Create a tween. Times are in seconds (1 sec = 4 frames)
+tween = Tween(3.0).wait(2).to(12.0, 5).wait(6.5).set(7.0).wait(7)
 
 # You can use len() to fund the length of the tween in frames
 print('Length = ', len(tween))
 
-# You can access the vale#ue for a particular frame using indexing []
+# You can access the value for a particular frame using indexing []
 # or the get() function.
-print('tween[7] = ', tween[5])
-print('tween[8] = ', tween.get(7))
+print('tween[6] (1.5 seconds) = ', tween[6])
+print('tween[12] (3 seconds) = ', tween.get(12))
+print('tween[80] (20 seconds) = ', tween.get(80))
 
 # The tween can be accessed as a sequence. Here we use a list
 # comprehension to convert the values to strings that print the
