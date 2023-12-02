4d4ddf1239506099e75b091c46ddf3bf1789294e
Martin <mcbride.martin@gmail.com>
2023-12-02 17:07:52 +0000

    Update setup.py for V23.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setup(name='generativepy',
-      version='4.0',
+      version='23.11',
       url='https://github.com/martinmcbride/generativepy',
       license='MIT',
       author='Martin McBride',
