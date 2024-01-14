dc2e6f5f882ce15326f651c3ea0101f3645e1369
Martin <mcbride.martin@gmail.com>
2024-01-14 13:15:36 +0000

    Imoplement rounding of division text

--- generativepy/povray.py
+++ generativepy/povray.py
@@ -103,6 +103,7 @@ class Axes3d:
         self.div_positions = None
         self.axis_thickness = 0.02
         self.color = Color("blue").light1
+        self.label_round = [1]*3
         self.texture = None
 
     def of_start(self, start):
@@ -243,7 +244,7 @@ class Axes3d:
         text = Text(
         "ttf",
              '"/usr/share/fonts/truetype/msttcorefonts/ariali.ttf"',
-             f'"{text}"',
+             f'"{text}"', # Povray requires "" around text
              0.1,
              0,
             self.texture,
@@ -261,15 +262,15 @@ class Axes3d:
         items = []
 
         for p in self.div_positions[0]:
-            s = str(p)
+            s = str(round(p, self.label_round[0]))
             p = self.transform_from_graph((p, 0, 0))[0]
             items.append(self._make_text_item(s, (p, 2.3, -2), (-0.5, 0, -1), (90, 0, -90)))
         for p in self.div_positions[1]:
-            s = str(p)
+            s = str(round(p, self.label_round[1]))
             p = self.transform_from_graph((0, p, 0))[1]
             items.append(self._make_text_item(s, (2, p, -2), (0, 0, -1)))
         for p in self.div_positions[2]:
-            s = str(p)
+            s = str(round(p, self.label_round[2]))
             p = self.transform_from_graph((0, 0, p))[2]
             items.append(self._make_text_item(s, (2, -2, p), (0.5, 0, 0)))
 
