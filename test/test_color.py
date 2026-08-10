41efe0129cc49ef46787ffeb56dd0e2642f769da
martin <mcbride.martin@gmail.com>
2026-08-10 14:13:41 +0100

    Add extra tests to color module to check for invalid params in constructor

--- test/test_color.py
+++ test/test_color.py
@@ -4,7 +4,7 @@ from generativepy.color import Color, make_colormap
 
 class TestColour(unittest.TestCase):
 
-    # Test RGB and RGBA colours
+    # Test RGB and RGBA colours, including invalid values
     def test_rgb_rgb_black(self):
         color = Color(0, 0, 0)
         self.assertEqual(color.rgb, (0, 0, 0))
@@ -29,11 +29,19 @@ class TestColour(unittest.TestCase):
         color = Color(1, 0.5, 0.25, 0.4)
         self.assertEqual(color.rgba, (1, 0.5, 0.25, 0.4))
 
+    def test_rgb_invalid_color(self):
+        with self.assertRaises(ValueError):
+            color = Color(None, 0.5, 0.25)
+
+    def test_rgba_invalid_color(self):
+        with self.assertRaises(ValueError):
+            color = Color(1, 0.5, 0.25, None)
+
+    # Test grey and grey+alpha modes
     def test_grey_rgb_black(self):
         color = Color(0)
         self.assertEqual(color.rgb, (0, 0, 0))
 
-    # Test grey and grey+alpha modes
     def test_grey_rgb_color(self):
         color = Color(0.25)
         self.assertEqual(color.rgb, (0.25, 0.25, 0.25))
@@ -53,6 +61,14 @@ class TestColour(unittest.TestCase):
     def test_greya_rgba_semi(self):
         color = Color(0.25, 0.4)
         self.assertEqual(color.rgba, (0.25, 0.25, 0.25, 0.4))
+
+    def test_grey_invalid_color(self):
+        with self.assertRaises(ValueError):
+            color = Color(None)
+
+    def test_greya_invalid_color(self):
+        with self.assertRaises(ValueError):
+            color = Color(1, None)
 
     # Test CSS and CSS plus alpha modes
     def test_css_rgb(self):
@@ -81,6 +97,14 @@ class TestColour(unittest.TestCase):
         self.assertAlmostEqual(color.rgba[1], 0.50196078)
         self.assertAlmostEqual(color.rgba[2], 0.44705882)
         self.assertAlmostEqual(color.rgba[3], 0.7)
+
+    def test_css_invalid_color_name(self):
+        with self.assertRaises(ValueError):
+            color = Color("invalidname")
+
+    def test_cssa_invalid_color_name(self):
+        with self.assertRaises(ValueError):
+            color = Color("invalidname", 0.5)
 
     # Test getters
     def test_get_r(self):
